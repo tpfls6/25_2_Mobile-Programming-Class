98033f78011f33d629b3e61a0426eb95dfaf717e
tpfls6 <tpfls@dongyang.ac.kr>
2025-11-07 12:04:46 +0900

    week09 : Student List App v0.7(RecyclerView)

--- AppWeek09/app/src/main/java/com/appweek09/data/Student.kt
+++ AppWeek09/app/src/main/java/com/appweek09/data/Student.kt
@@ -3,13 +3,13 @@ package com.appweek09.data
 import java.util.Date
 import java.util.UUID
 
-data class Student(
+class Student(
     val name: String,
     val id: String = UUID.randomUUID().toString(),
     val department: String = "Computer Science",
     val grade: String = "2nd Year",
     val email: String = "",
     val addedDate: Date = Date()
-){
+) {
     override fun toString(): String = name
 }
