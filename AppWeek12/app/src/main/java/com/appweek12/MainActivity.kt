21633511cd225f0273efb31fbef4a67b4c43ab14
tpfls6 <tpfls@dongyang.ac.kr>
2025-11-28 10:50:41 +0900

    week12: Counter App v0.4

---
+++ AppWeek12/app/src/main/java/com/appweek12/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.appweek12
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.appweek12.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("count", 0)
+            updateCountDisplay()
+        }
+
+        setupListeners()
+        updateCountDisplay()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("count", count)
+    }
+
+    private fun setupListeners() {
+        binding.buttonPlus.setOnClickListener {
+            count++
+            updateCountDisplay()
+        }
+
+        binding.buttonMinus.setOnClickListener {
+            count--
+            updateCountDisplay()
+        }
+
+        binding.buttonReset.setOnClickListener {
+            count = 0
+            updateCountDisplay()
+        }
+
+        binding.buttonPlus10.setOnClickListener {
+            count += 10
+            updateCountDisplay()
+        }
+    }
+
+    private fun updateCountDisplay() {
+        binding.textViewCount.text = count.toString()
+
+        when {
+            count > 0 -> binding.textViewCount.setTextColor(Color.GREEN)
+            count < 0 -> binding.textViewCount.setTextColor(Color.RED)
+            else -> binding.textViewCount.setTextColor(Color.BLACK)
+        }
+    }
+}
